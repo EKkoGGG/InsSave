1e444013319e3a83dcbb2b05d92511c523bbb972
JJ <330564052@qq.com>
2020-03-13 17:26:39 +0800

    * demo完成

--- InsSave/Controllers/HomeController.cs
+++ InsSave/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using InsSave.Models;
+using AngleSharp.Html.Parser;
+using System.Net.Http;
+using AngleSharp.Html.Dom;
 
 namespace InsSave.Controllers
 {
     public class HomeController : Controller
     {
+
+        [HttpPost]
+        public IActionResult Test(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                //string Url = "https://www.instagram.com/p/B7PzQc_HYtO/?utm_source=ig_web_copy_link";
+                var source = client.GetStringAsync(url).Result;
+                var parser = new HtmlParser();
+                var document = parser.ParseDocument(source);
+                var s1 = document.All[37] as IHtmlMetaElement;
+                ViewBag.str = s1.Content;
+
+                return View();
+            }
+        }
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
